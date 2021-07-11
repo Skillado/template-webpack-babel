9f5b98d43885c97244370591eea06dd14716f369
Guilherme Drumond <drumond.guilherme@hotmail.com>
2021-07-11 17:30:57 -0300

    integration webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+  mode:"development",
+  devtool:"eval-source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+   contentBase: path.resolve(__dirname, "public")
+  },
+  plugins: [
+   new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+   })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};

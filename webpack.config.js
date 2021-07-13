d10e436bcd868058a185bc1e8c6f9ce2344a4d1d
Guilherme Drumond <drumond.guilherme@hotmail.com>
2021-07-13 18:14:31 -0300

    Integration of React FastRefresh into the project!

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const isDevelopment = process.env.NODE_ENV !== "production";
-
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   mode: isDevelopment?"development":"production",
@@ -12,13 +12,15 @@ module.exports = {
     filename: "bundle.js",
   },
   devServer: {
-   contentBase: path.resolve(__dirname, "public")
+   contentBase: path.resolve(__dirname, "public"),
+   hot: true,
   },
   plugins: [
+   isDevelopment && new ReactRefreshWebpackPlugin(),
    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
    })
-  ],
+  ].filter(Boolean),
   resolve: {
     extensions: [".js", ".jsx"],
   },
@@ -27,7 +29,14 @@ module.exports = {
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader:"babel-loader",
+          options:{
+            plugins:[
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        },
       },
       {
         test: /\.scss$/,
